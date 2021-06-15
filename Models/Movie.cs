4e1f0594e7136d268ae8efa6a32d575f99112894
Ezra Akeba <akebaezra@gmail.com>
2021-06-15 11:19:20 +0300

    Fix column data type for price

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -13,6 +13,8 @@ namespace MvcMovie.Models
         [DataType(DataType.Date)]
         public DateTime ReleaseDate {get; set; }
         public string Genre {get; set; }
+        
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price {get; set; }
     }
 }
