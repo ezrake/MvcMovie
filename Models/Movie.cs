5f9936d608828d0fc573cf79c866df742c289f65
Ezra Akeba <akebaezra@gmail.com>
2021-06-15 11:05:57 +0300

    Change ReleaseDate to Release Date in views
    Use [Display (Name = )]  data annotation to correct

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -8,6 +9,7 @@ namespace MvcMovie.Models
         public int Id {get; set;}
         public string Title {get; set;}
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate {get; set; }
         public string Genre {get; set; }
